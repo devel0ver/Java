0e1b78484f4ad19ea3d26c1404f18b121a3038e0
devel0ver <shayaahmed06@gmail.com>
2022-02-07 13:58:15 -0500

    first commit

---
+++ fundamentals/first_java_program/Info.java
@@ -0,0 +1,11 @@
+public class Info {
+    public static void main(String[] args) {
+        String name = "Shaya Ahmed";
+        int age = 20;
+        String location = "Brooklyn, NY";
+
+        System.out.println("My name is " + name);
+        System.out.printf("I am %s years old\n", age);
+        System.out.println("My hometown is " + location);
+    }
+}
