120ed9882c953137d6ba3fc3bda50d51c42e1730
devel0ver <shayaahmed06@gmail.com>
2022-02-10 11:09:26 -0500

    added

--- Algos/removeDupes.js
+++ Algos/removeDupes.js
@@ -21,3 +21,59 @@ function removeDupes(arr){
 }
 
 console.log(removeDupes([3,6,7,3,2,7,7,9,0,1])) //output-> [3,6,7,2,9,0,1]
+
+
+
+// Recursive 
+
+// let x = [
+//     [12, 34, 45, 56],
+//     [98, 67, 76, 65],
+//     [56, 67, 78, 89],
+//     [54, 43, 32, 21]
+// ]
+
+// let y = [
+//     [67, 78],
+//     [43, 32]
+// ]
+
+
+// const matrixSearch = (bigArr, smallArr, row=0)=>{
+//     console.log({row})
+//     const width = smallArr[0].length; //2
+//     const height = smallArr.length; //2
+//     const start = smallArr[0][0]; //67
+//     let found = false;
+//     if(bigArr[row]){
+//         for(var i= 0; i<bigArr[row].length; i++){
+//             console.log(bigArr[row][i])
+//             if(bigArr[row][i] === start){
+//                 found = true;
+//                 break;
+//             } 
+//         }
+//         if(!found){
+//             return matrixSearch(bigArr, smallArr, row+1)
+//         }
+
+//     }else{
+//         return false;
+//     }
+//     //here we are at a point where the small value is found
+//     // console.log("found first value at i, i is->", row, i, found)
+//     for(let j = 0; j<smallArr.length; j++){
+//         for(let k = 0; k<smallArr[j].length; k++){
+//             console.log(smallArr[j][k], bigArr[row+j][i+k])
+//             if(smallArr[j][k] != bigArr[row+j][i+k]){
+//                 found = false;
+//                 return matrixSearch(bigArr, smallArr, row+1)
+//             }
+//         }
+//     }
+//     return true;
+
+// }
+
+
+// console.log(matrixSearch(x,y))
