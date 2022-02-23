2f78a024ee506e6b036835249bc4e834d4a90bc1
devel0ver <shayaahmed06@gmail.com>
2022-02-23 10:56:49 -0500

    added findMin and findMax

--- Algos/w3d4algos.js
+++ Algos/w3d4algos.js
@@ -15,54 +15,67 @@ class BST{
 
     insert(valueInput){
         //create a node with thee value input and insert it into the tree using the rules of a bst
-        let nToAdd = new Node(valueInput);
-
-        if(this.root==null){
-            this.root=nToAdd;
-            return this;
+        let newNode = new Node(valueInput)
+        //if the root is null we want to insert a new node and make it the valueInput.
+        if(this.root == null){
+            //make root equal to new node
+            this.root = newNode
+            return this
         }
-        else{
-
-            let searchToken = this.root;
-
-            while(searchToken !=null){
-
-                if(searchToken.value>nToAdd.value){
-                    if(searchToken.left==null){
-                        searchToken.left=nToAdd;
-                        return this ;
-                    }
-                    else{
-                        searchToken = searchToken.left;
+        else {
+            let current = this.root
+            while(current != null){
+                if(valueInput > current.value){
+                    if(current.right == null){
+                        current.right = newNode
+                        return this
+                    }else{
+                        current = current.right
                     }
                 }
-                else if ( searchToken.value<nToAdd.value){
-                    if(searchToken.right ==null){
-                        searchToken.right=nToAdd;
-                        return this;
-                    }
-                    else{
-                        searchToken=searchToken.right;
-                    }
-                }else if(searchToken.value == nToAdd.value ){
-                    if(searchToken.left==null){
-                        searchToken.left=nToAdd;
-                        return this;
-                    }
-                    else{
-                        searchToken = searchToken.left;
+                else{
+                    if(current.left == null){
+                        current.left = newNode
+                        return this
+                    }else{
+                        current = current.left
                     }
                 }
             }
         }
+        return this
+
     }
 
+    findMin(){
+        //create a variable called current node and start it at the root
+        let currentNode = this.root 
+        //move current until current.left is null 
+        //while current.left is not null move current to the left node
+        while(currentNode.left != null){
+            currentNode = currentNode.left
+        }
+        //after the while loop is done, current will be at the node containing the minimum value, then return current.value
+        return currentNode.value
+    }
 
+    findMax(){
+        //create a variable called current node and start it at the root
+        let currentNode = this.root
+        //move current until current.right is null
+        //while current.right is not null move current to the right node 
+        while(currentNode.right != null){
+            currentNode = currentNode.right
+        }
+        //after the while loop is done, current will be at the node containing the maximum value, then currentNode.value
+        return currentNode.value
+    }
 }
 
 
 let bst1 = new BST()
 
-bst1.insert(5)
+bst1.insert(5).insert(3).insert(12).insert(10).insert(1).insert(4).insert(7).insert(14).insert(2)
 
 console.log(bst1)
+console.log(bst1.findMax())
